619e62cfc1a408516e43863274fbd4e442cf8dab
Rod3052 <rscott@westell.com>
2023-08-26 00:13:00 -0400

    some more stuff

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
+* Start Bootstrap - Jannas Portfolio v1 (https://startbootstrap.com/theme/stylish-portfolio)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/alise-portfolio/blob/master/LICENSE)
 */
 window.addEventListener('DOMContentLoaded', event => {
 
